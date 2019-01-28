9aedbad149312051b975496cd959381933c7a503
Raine Wallace <rainebeaudubs@gmail.com>
2019-01-28 15:36:34 -0500

    I think I have correctly 'declared' my routes. I'm not sure. It seemed too easy.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import './App.css';
 import { Home, About, Contact, Navigation } from './components';
-
+import { Route } from 'react-router-dom';
 
 
 const App = () => (
   <div>
     <Navigation />
+    <div>
+      <Route path="/" component={Home} />
+      <Route path="/about" component={About} />
+      <Route path="/contact" component={Contact} />
+    </div>
   </div>
+
 );
 
 export default App;
